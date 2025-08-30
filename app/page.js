049ede5dba1a9ca827c4c221e957bb4d2d2e1cc0
migeles <kikjidetes@gmail.com>
2025-08-30 12:46:59 +0700

    1

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Hero from "@/Components/Hero/Hero";
-import Porto from "../Components/Porto/Porto";
 import Navbar from "@/Components/Navbar/Navbar";
 import Goals from "@/Components/Goals/Goals";
+import Porto from "../Components/Porto/Porto";
 import Service from "@/Components/Service/Service";
 import Footer from "@/Components/Footer/Footer";
 
